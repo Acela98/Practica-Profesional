1fa08258590fc9d867d06816210c03bc4769e119
Leila <leilabarboza2016@gmail.com>
2023-08-04 17:08:43 -0400

    Adision de riterios de busquedas

--- ReservaCanchas/ExamenFinal/polls/admin.py
+++ ReservaCanchas/ExamenFinal/polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Usuario
+from .models import Cancha
+from .models import Sede
+from .models import Reserva
 
-# Register your models here.
+class Usuario(admin.ModelAdmin):
+    ordering = ['-nombre']
+    search_fields=('ci','nombre','apellido')
+
+class Sede(admin.ModelAdmin):
+    ordering = ['-nombre']
+    search_fields=('nombre',)
+
+class Reserva(admin.ModelAdmin):
+    ordering = ['-fecha']
+    search_fields=('fecha',)
+
+class Cancha(admin.ModelAdmin):
+    ordering = ['-nombre']
+    search_fields=('nombre',)
+
