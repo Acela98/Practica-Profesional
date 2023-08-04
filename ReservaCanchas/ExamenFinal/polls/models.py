e41284f7bb3cf41ab776b33faea7bf59c8d1d2c9
Leila <leilabarboza2016@gmail.com>
2023-08-04 16:44:08 -0400

    nueva clase

--- ReservaCanchas/ExamenFinal/polls/models.py
+++ ReservaCanchas/ExamenFinal/polls/models.py
@@ -6,11 +6,22 @@ class Usuario(models.Model):
    
     correo = models.CharField(max_length=50)
     telefono = models.CharField(max_length=20)
-    fecha_inicio = models.DateField()
+    fecha_registro = models.DateField()
+    fecha_nac = models.DateField()
     ci = models.CharField(max_length=20)
-    estado = models.CharField(max_length=20)
+    tipo = models.CharField(max_length=20)
     direccion = models.CharField(max_length=50)
+    contrasena = models.CharField(max_length=20)
     def __str__(self):
         return f"{self.nombre} - {self.apellido} - {self.correo}"
     
+class Sede(models.Model):
+    nombre = models.CharField(blank=True, null=True, max_length= 50)
+    direccion = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=20)
+    estado = models.CharField(max_length=20)
+
+    
+
+
 
