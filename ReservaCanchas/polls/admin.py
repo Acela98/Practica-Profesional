47c4f96ca40c445f287185e14cd65e9a957cf7b0
Leila <leilabarboza2016@gmail.com>
2023-08-04 17:32:28 -0400

    adision de criterios de busqueda

--- ReservaCanchas/polls/admin.py
+++ ReservaCanchas/polls/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Usuario
+from .models import Cancha
+from .models import Sede
+from .models import Reserva
+
+class Usuario(admin.ModelAdmin):
+    ordering = ['-nombre']
+    search_fields=('ci','nombre','apellido')
+
+class Sede(admin.ModelAdmin):
+    ordering = ['-nombre']
+    search_fields=('nombre',)
+
+class Reserva(admin.ModelAdmin):
+    ordering = ['-fecha']
+    search_fields=('fecha',)
+
+class Cancha(admin.ModelAdmin):
+    ordering = ['-nombre']
+    search_fields=('nombre',)
+
+
